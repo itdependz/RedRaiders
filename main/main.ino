1c69c0007c4cd4ede830592bec13c2ba9e582a29
Samarth Das <samarthdas81@gmail.com>
2023-12-13 20:45:59 -0500

    driving, inverted

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,15 @@ BluetoothSerial bluetooth;
 // If your robot has more than a drivetrain and one servo, add those actuators here 
 NoU_Motor top(3);
 NoU_Motor bottom(2);
+NoU_Motor frontLeftMotor(1);
+NoU_Motor frontRightMotor(6);
+NoU_Motor rearLeftMotor(4);
+NoU_Motor rearRightMotor(5);
+
+NoU_Drivetrain drivetrain(&frontLeftMotor, &frontRightMotor, &rearLeftMotor, &rearRightMotor);
+
+float leftThrottle, rightThrottle, servoAxis;
+long lastTimePacketReceived = 0;
 // This creates the drivetrain object, you shouldn't have to mess with this
 // NoU_Drivetrain drivetrain(&frontLeftMotor, &frontRightMotor, &rearLeftMotor, &rearRightMotor);
 
@@ -32,16 +41,28 @@ void setup() {
 // No need to mess with this code
   RSL::initialize();
   RSL::setState(RSL_ENABLED);
+
+  //drive train setup
+  frontLeftMotor.setInverted(false);
+  frontRightMotor.setInverted(true);
+  rearLeftMotor.setInverted(false);
+  rearRightMotor.setInverted(true);
+
 }
 
 void loop() {
 // Here we define the variables we use in the loop
 int power = 0;
 
-// Here we decide what the throttle and rotation direction will be based on gamepad inputs   
+// Here we decide what the thr ottle and rotation direction will be based on gamepad inputs   
   if (AlfredoConnect.getGamepadCount() >= 1) {
     float power = -AlfredoConnect.getAxis(0, 1);
+    float turn = AlfredoConnect.getAxis(0, 2);
+
+    drivetrain.arcadeDrive(power, turn);
+
     
+    //intake
     top.set(power);
     bottom.set(power);
 
